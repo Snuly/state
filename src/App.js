d8972c3ced4ad9ad5c8a6304188d4a4da3618a68
Snuly <vortifexyt@gmail.com>
2023-10-19 09:46:42 +0300

    + and - fixed to work with given value

--- src/App.js
+++ src/App.js
@@ -8,16 +8,25 @@ import { useState } from "react";
 
 function App() {
   const [result, setResult] = useState(0);
+  const [ratioChange, setRatioChange] = useState(1);
 
   function addRatio() {
-    setResult(result + 1);
-    console.log("Funkcija izsaukta");
-    console.log("result ir " + result);
+    setResult(result + parseInt(ratioChange));
+  }
+
+  function subtractRatio() {
+    setResult(result - ratioChange);
+  }
+
+  function handleRatioChange(event) {
+    setRatioChange(event.target.value)
   }
 
   return (
     <div className="App">
+      <input type="number" value={ratioChange} onChange={handleRatioChange}></input>
       <button onClick={addRatio}>GANG +1 ratio</button>
+      <button onClick={subtractRatio}>GANG -1 ratio</button>
       <h1>{result}</h1>
     </div>
   );
